f5bd31b5b5dd38f1083f69ddbd330d09d2818bc9
SpaceWorpDeeno <Yayplez@gmail.com>
2024-09-03 12:28:09 +1000

    Initial Commit

---
+++ Something/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Something
+//
+//  Created by Cheston Lam on 3/9/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
